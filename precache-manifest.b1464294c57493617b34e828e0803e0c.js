1837a5ed9c6fd67ee528630e2b1016559072700e
adwaitp <adwait.purao@spit.ac.in>
2023-07-19 19:17:25 +0530

    Updates

---
+++ precache-manifest.b1464294c57493617b34e828e0803e0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8abb9223f1d8c6e5a45",
+    "url": "/Task-Manager-App-using-React/static/css/main.ffe4b308.chunk.css"
+  },
+  {
+    "revision": "a8abb9223f1d8c6e5a45",
+    "url": "/Task-Manager-App-using-React/static/js/main.a8abb922.chunk.js"
+  },
+  {
+    "revision": "e0a578bb5944d36fcb61",
+    "url": "/Task-Manager-App-using-React/static/js/1.e0a578bb.chunk.js"
+  },
+  {
+    "revision": "00841f61a70e77f29aa3",
+    "url": "/Task-Manager-App-using-React/static/js/runtime~main.00841f61.js"
+  },
+  {
+    "revision": "1fe34c016ca19cb5fde78f2cdba72edc",
+    "url": "/Task-Manager-App-using-React/index.html"
+  }
+];
